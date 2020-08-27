a849cef2cae849719265706cad4715c0dca4e705
rajesh.radhakrishnan <rajesh.radhakrishnan@puppet.com>
2020-08-26 19:56:10 -0700

    added check for bolt exec

--- run.sh
+++ run.sh
@@ -6,6 +6,14 @@ if [ $# -ne 3 ]
     echo "Usage: run.sh <PE FQDN> <PE Node User name> <PE Node Private Key>"
     exit
 fi
+
+if ! command -v bolt &> /dev/null
+then
+    echo "Error: bolt executable could not be found"
+    echo "Info: Bolt installation instructions are at https://puppet.com/docs/bolt/latest/bolt_installing.html"
+    exit
+fi
+
 echo "Info: Downloading pe_quick_data module from https://forge.puppet.com"
 bolt puppetfile install
 echo "Info: Running the pe_quick_data module. The collected data will be in /tmp"
